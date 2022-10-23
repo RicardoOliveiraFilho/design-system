2aa3a38c3a94fba67aaf96d8697be161ebba1eca
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-10-23 10:36:12 -0300

    Configurando pacote do React

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@ricardo-oliveira-ignite-ui/tokens'
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@ricardo-oliveira-ignite-ui/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Olá Mundo</h1>
+}
