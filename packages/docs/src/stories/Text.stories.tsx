da96ccdf2fcde942bb0926cf3bbc8764b95c8a01
RicardoOliveiraFilho <ricardo.oliveira1307@gmail.com>
2022-10-30 23:54:30 -0300

    Componente: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ricardo-oliveira-ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui modi sint architecto fugiat dolor sunt velit, iste, dignissimos asperiores ducimus non. Ipsam quod animi aut quidem possimus est, sint ullam.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong', // O 'as' substitui a tag padrão do componente, no caso um 'p', pelo que foi informado a ele!
+  }
+}
